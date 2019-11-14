dec668de8aba411ff3ffa1fcfcfd3135e104c515
Luiz Camargos <46843342+LuizCamargos@users.noreply.github.com>
2019-11-13 21:57:23 -0300

    [UI] Tela Inicial
    Criado uma base para a tela inicial

--- lib/Pages/HomeScreen.dart
+++ lib/Pages/HomeScreen.dart
@@ -1,1 +1,183 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'DetailProblemScreen.dart';
 
+class TelaHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Dia 24/10",
+      home: Scafold(),
+    );
+  }
+}
+
+class Scafold extends StatefulWidget {
+  @override
+  _ScafoldState createState() => _ScafoldState();
+}
+
+class _ScafoldState extends State<Scafold> {
+  final GlobalKey<ScaffoldState> _scafoldkey = new GlobalKey<ScaffoldState>();
+  Color cor = Color(0xffF3F3F3);
+  String selecionado = "";
+  int primeiro = 1, segundo = 1, soma = 0;
+
+  String _counter, _value = "";
+
+  Future _incrementCounter() async {
+    _counter =
+        await FlutterBarcodeScanner.scanBarcode("#F3F3F3", "Cancelar", true);
+    setState(() {
+      _value = _counter;
+      //TODO, abrir uma tela aqui, logo depois que sair da seleção da sala
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => DetailScreen()));
+    });
+  }
+
+  void setcor(String x) {
+    setState(() {
+      selecionado = x;
+      _showSnackBar();
+    });
+  }
+
+  void fibonacci() {
+    setState(() {
+      primeiro = segundo;
+      segundo = soma;
+      soma = primeiro + segundo;
+    });
+  }
+
+  _showSnackBar() {
+    print("Show snackbar $selecionado");
+    //inicializacao do snack bar
+    final snackBar = new SnackBar(
+      //content: new Text("Selecionado $selecionado"),
+      content: new Text("Selecionado $_value"),
+      duration: const Duration(milliseconds: 5000),
+      elevation: 1,
+      action: SnackBarAction(
+        label: "OK",
+        onPressed: () => {},
+      ),
+    );
+    _scafoldkey.currentState.showSnackBar(snackBar);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      key: _scafoldkey,
+      body: Column(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(12),
+              bottomRight: Radius.circular(12),
+            ),
+            child: Container(
+              color: Color(0xff133984),
+              height: 85,
+              width: MediaQuery.of(context).size.width,
+            ),
+          ),
+          Container(
+            color: Color(0xffF3F3F3),
+            padding: const EdgeInsets.only(top: 8),
+            child: Material(
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                    title: Text(
+                      "Ar Condicionado",
+                      style: TextStyle(color: Color(0xff8A8A8A)),
+                    ),
+                    leading: Image.asset(
+                      'assets/images/air_conditioner.png',
+                      height: 32,
+                      width: 32,
+                    ),
+                    onTap: () {
+                      setcor("Ar Condicionado");
+                    },
+                  ),
+                  ListTile(
+                    title: Text(
+                      "Projetor",
+                      style: TextStyle(color: Color(0xff8A8A8A)),
+                    ),
+                    leading: Image.asset(
+                      'assets/images/projector.png',
+                      height: 32,
+                      width: 32,
+                    ),
+                    onTap: () {
+                      setcor("Projetor");
+                    },
+                  ),
+                  ListTile(
+                    title: Text(
+                      "Computador",
+                      style: TextStyle(color: Color(0xff8A8A8A)),
+                    ),
+                    leading: Image.asset(
+                      'assets/images/computer.png',
+                      height: 32,
+                      width: 32,
+                    ),
+                    onTap: () {
+                      setcor("Computador");
+                    },
+                  ),
+                  ListTile(
+                    title: Text(
+                      "Som",
+                      style: TextStyle(color: Color(0xff8A8A8A)),
+                    ),
+                    leading: Image.asset(
+                      'assets/images/loudspeaker.png',
+                      height: 32,
+                      width: 32,
+                    ),
+                    onTap: () {
+                      setcor("Som");
+                    },
+                  ),
+                  ListTile(
+                    title: Text(
+                      "Impressora",
+                      style: TextStyle(color: Color(0xff8A8A8A)),
+                    ),
+                    leading: Image.asset(
+                      'assets/images/printer.png',
+                      height: 32,
+                      width: 32,
+                    ),
+                    onTap: () {
+                      setcor("Impressora");
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.arrow_forward_ios,
+                    ),
+                    title: Text(_value),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        elevation: 0,
+        label: Text("Scan QR"),
+        icon: Icon(Icons.camera_alt),
+        onPressed: _incrementCounter,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,184 +1,73 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
-import 'Pages/DetailProblemScreen.dart';
 
-void main() => runApp(TelaHome());
+void main() => runApp(MainScreen());
 
-class TelaHome extends StatelessWidget {
+class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Dia 24/10",
-      home: Scafold(),
+      home: MainScreenUI(),
+      color: Color(0xffF3F3F3),
     );
   }
 }
 
-class Scafold extends StatefulWidget {
-  @override
-  _ScafoldState createState() => _ScafoldState();
-}
-
-class _ScafoldState extends State<Scafold> {
-  final GlobalKey<ScaffoldState> _scafoldkey = new GlobalKey<ScaffoldState>();
-  Color cor = Color(0xffF3F3F3);
-  String selecionado = "";
-  int primeiro = 1, segundo = 1, soma = 0;
-
-  String _counter, _value = "";
-
-  Future _incrementCounter() async {
-    _counter =
-        await FlutterBarcodeScanner.scanBarcode("#F3F3F3", "Cancelar", true);
-    setState(() {
-      _value = _counter;
-      //TODO, abrir uma tela aqui, logo depois que sair da seleção da sala
-      Navigator.of(context)
-          .push(MaterialPageRoute(builder: (context) => DetailScreen()));
-    });
-  }
-
-  void setcor(String x) {
-    setState(() {
-      selecionado = x;
-      _showSnackBar();
-    });
-  }
-
-  void fibonacci() {
-    setState(() {
-      primeiro = segundo;
-      segundo = soma;
-      soma = primeiro + segundo;
-    });
-  }
-
-  _showSnackBar() {
-    print("Show snackbar $selecionado");
-    //inicializacao do snack bar
-    final snackBar = new SnackBar(
-      //content: new Text("Selecionado $selecionado"),
-      content: new Text("Selecionado $_value"),
-      duration: const Duration(milliseconds: 5000),
-      elevation: 1,
-      action: SnackBarAction(
-        label: "OK",
-        onPressed: () => {},
-      ),
-    );
-    _scafoldkey.currentState.showSnackBar(snackBar);
-  }
-
+class MainScreenUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      key: _scafoldkey,
-      body: Column(
-        children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(12),
-              bottomRight: Radius.circular(12),
-            ),
-            child: Container(
-              color: Color(0xff133984),
-              height: 85,
-              width: MediaQuery.of(context).size.width,
-            ),
-          ),
-          Container(
-            color: Color(0xffF3F3F3),
-            padding: const EdgeInsets.only(top: 8),
-            child: Material(
-              child: Column(
-                children: <Widget>[
-                  ListTile(
-                    title: Text(
-                      "Ar Condicionado",
-                      style: TextStyle(color: Color(0xff8A8A8A)),
+    return Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Card(
+              color: Color(0xffdcdcdc),
+              elevation: 3,
+              child: Container(
+                padding: const EdgeInsets.all(12),
+                height: 180,
+                width: 128,
+                child: Center(
+                  child: Text(
+                    "São José",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff004496),
+                      fontSize: 22,
                     ),
-                    leading: Image.asset(
-                      'assets/images/air_conditioner.png',
-                      height: 32,
-                      width: 32,
-                    ),
-                    onTap: () {
-                      setcor("Ar Condicionado");
-                    },
                   ),
-                  ListTile(
-                    title: Text(
-                      "Projetor",
-                      style: TextStyle(color: Color(0xff8A8A8A)),
-                    ),
-                    leading: Image.asset(
-                      'assets/images/projector.png',
-                      height: 32,
-                      width: 32,
-                    ),
-                    onTap: () {
-                      setcor("Projetor");
-                    },
-                  ),
-                  ListTile(
-                    title: Text(
-                      "Computador",
-                      style: TextStyle(color: Color(0xff8A8A8A)),
-                    ),
-                    leading: Image.asset(
-                      'assets/images/computer.png',
-                      height: 32,
-                      width: 32,
-                    ),
-                    onTap: () {
-                      setcor("Computador");
-                    },
-                  ),
-                  ListTile(
-                    title: Text(
-                      "Som",
-                      style: TextStyle(color: Color(0xff8A8A8A)),
-                    ),
-                    leading: Image.asset(
-                      'assets/images/loudspeaker.png',
-                      height: 32,
-                      width: 32,
-                    ),
-                    onTap: () {
-                      setcor("Som");
-                    },
-                  ),
-                  ListTile(
-                    title: Text(
-                      "Impressora",
-                      style: TextStyle(color: Color(0xff8A8A8A)),
-                    ),
-                    leading: Image.asset(
-                      'assets/images/printer.png',
-                      height: 32,
-                      width: 32,
-                    ),
-                    onTap: () {
-                      setcor("Impressora");
-                    },
-                  ),
-                  ListTile(
-                    leading: Icon(
-                      Icons.arrow_forward_ios,
-                    ),
-                    title: Text(_value),
-                  )
-                ],
+                ),
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.only(left: 30),
+            ),
+            Card(
+              color: Color(0xffdcdcdc),
+              elevation: 3,
+              child: Container(
+                padding: const EdgeInsets.all(12),
+                height: 180,
+                width: 128,
+                child: Center(
+                  child: Text(
+                    "Liceu",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                        color: Color(0xff004496)),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        elevation: 0,
-        label: Text("Scan QR"),
-        icon: Icon(Icons.camera_alt),
-        onPressed: _incrementCounter,
+      bottomNavigationBar: Container(
+        alignment: Alignment.center,
+        child: Image.asset('assets/images/logo_unisal.png'),
+        width: 200,
+        height: 120,
       ),
     );
   }
