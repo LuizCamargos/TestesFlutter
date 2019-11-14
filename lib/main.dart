cfe8a3812d66f16d74b53bb8009a57caa8c0f80e
Luiz Camargos <46843342+LuizCamargos@users.noreply.github.com>
2019-11-14 17:42:11 -0300

    [UI Inicial] Botões atualizados
    agora os botões já tem ações e já estão prontos para passarem parametros

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,24 @@ class MainScreen extends StatelessWidget {
     return MaterialApp(
       home: MainScreenUI(),
       color: Color(0xffF3F3F3),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MainScreenUI extends StatelessWidget {
+class MainScreenUI extends StatefulWidget {
+  @override
+  _MainScreenUIState createState() => _MainScreenUIState();
+}
+
+class _MainScreenUIState extends State<MainScreenUI> {
+  String unidadeUnisal;
+
+  void _setUnidade(String unidade) {
+    unidadeUnisal = unidade;
+    print(unidadeUnisal);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,17 +36,24 @@ class MainScreenUI extends StatelessWidget {
             Card(
               color: Color(0xffdcdcdc),
               elevation: 3,
-              child: Container(
-                padding: const EdgeInsets.all(12),
-                height: 180,
-                width: 128,
-                child: Center(
-                  child: Text(
-                    "São José",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xff004496),
-                      fontSize: 22,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(8),
+                onTap: () => _setUnidade("SJ"),
+                child: Container(
+                  padding: const EdgeInsets.all(12),
+                  height: 180,
+                  width: 128,
+                  child: Center(
+                    child: Text(
+                      "São José",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xff004496),
+                        fontSize: 22,
+                      ),
                     ),
                   ),
                 ),
@@ -45,17 +65,25 @@ class MainScreenUI extends StatelessWidget {
             Card(
               color: Color(0xffdcdcdc),
               elevation: 3,
-              child: Container(
-                padding: const EdgeInsets.all(12),
-                height: 180,
-                width: 128,
-                child: Center(
-                  child: Text(
-                    "Liceu",
-                    style: TextStyle(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(8),
+                onTap: () => _setUnidade("Liceu"),
+                child: Container(
+                  padding: const EdgeInsets.all(12),
+                  height: 180,
+                  width: 128,
+                  child: Center(
+                    child: Text(
+                      "Liceu",
+                      style: TextStyle(
                         fontWeight: FontWeight.bold,
+                        color: Color(0xff004496),
                         fontSize: 22,
-                        color: Color(0xff004496)),
+                      ),
+                    ),
                   ),
                 ),
               ),
@@ -66,8 +94,8 @@ class MainScreenUI extends StatelessWidget {
       bottomNavigationBar: Container(
         alignment: Alignment.center,
         child: Image.asset('assets/images/logo_unisal.png'),
-        width: 200,
         height: 120,
+        margin: const EdgeInsets.only(bottom: 8),
       ),
     );
   }
