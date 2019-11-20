a77aed676558a698f57a66d393e5515a29ab1950
Luiz Camargos <luiz.camargos@hotmail.com>
2019-11-20 13:10:13 -0300

    Desenho da tela 2 iniciado
    Primeiro upgrade na tela 2

--- lib/Pages/DetailProblemScreen.dart
+++ lib/Pages/DetailProblemScreen.dart
@@ -4,8 +4,51 @@ class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Text("Tela 2"),
+      home: telaScreen(),
+    );
+  }
+}
+
+class telaScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffF3F3F3),
+      body: Column(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(12),
+              bottomRight: Radius.circular(12),
+            ),
+            child: Container(
+              color: Color(0xff133984),
+              height: 85,
+              width: MediaQuery.of(context).size.width,
+            ),
+          ),
+          Chip(
+            label: Text(
+              "Temperatura",
+              style: TextStyle(color: Color(0xff4D4D4D)),
+            ),
+            backgroundColor: Color(0xffFFFFFF),
+          ),
+          Chip(
+            label: Text(
+              "Temperatura",
+              style: TextStyle(color: Color(0xff4D4D4D)),
+            ),
+            backgroundColor: Color(0xffFFFFFF),
+          ),
+          Chip(
+            label: Text(
+              "Temperatura",
+              style: TextStyle(color: Color(0xff4D4D4D)),
+            ),
+            backgroundColor: Color(0xffFFFFFF),
+          ),
+        ],
       ),
     );
   }
